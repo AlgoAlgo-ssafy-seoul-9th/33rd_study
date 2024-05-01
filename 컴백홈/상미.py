ab7f0004efcdcd69e05bffda44690728b4b2bed0
sangmihwang <01071184366a@gmail.com>
2024-05-01 17:34:59 +0900

    상미: 컴백홈

--- 컴백홈/상미.py
+++ 컴백홈/상미.py
@@ -2,6 +2,7 @@ import sys
 input = sys.stdin.readline
 
 def sol(x, y, dis, visited):
+    global ans
     # print(x, y, dis, visited)
     if x == 0 and y == C-1:
         if dis == K:
@@ -12,22 +13,26 @@ def sol(x, y, dis, visited):
     if 0 <= x-1 < R and not visited[x-1][y] and  arr[x-1][y] != 'T': 
         visited[x-1][y] = 1
         sol(x-1, y, dis+1, visited)
-    elif 0 <= y+1 < C and not visited[x][y+1] and arr[x][y+1] != 'T':
+        visited[x-1][y] = 0
+    if 0 <= y+1 < C and not visited[x][y+1] and arr[x][y+1] != 'T':
         visited[x][y+1] = 1
         sol(x, y+1, dis+1, visited)
-    elif 0 <= x+1 < R and not visited[x+1][y] and arr[x+1][y] != 'T':
+        visited[x][y+1] = 0
+    if 0 <= x+1 < R and not visited[x+1][y] and arr[x+1][y] != 'T':
         visited[x+1][y] = 1
         sol(x+1, y, dis+1, visited)
-    elif 0 <= y-1 < C and not visited[x][y-1] and arr[x][y-1] != 'T':
+        visited[x+1][y] = 0
+    if 0 <= y-1 < C and not visited[x][y-1] and arr[x][y-1] != 'T':
         visited[x][y-1] = 1
         sol(x, y-1, dis+1, visited)
-    else:
-        return
+        visited[x][y-1] = 0
+    
 
 R, C, K = map(int, input().split())
 arr = [list(map(str, input().strip())) for _ in range(R)]
 ans = 0
 visited = [[0]*C for _ in range(R)]
-sol(R-1, 0, 0, visited)
+visited[R-1][0] = 1
+sol(R-1, 0, 1, visited)
 
 print(ans)
